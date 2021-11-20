610ea0e46d7698ac3fbfe0fc6c7a1378de8ab1ee
Adrian <adrianaquino.aquino33@gmail.com>
2021-11-20 10:03:52 +0000

    Remove other algorithm

--- Pathfind.h
+++ Pathfind.h
@@ -8,7 +8,6 @@ public:
 	Pathfind();
 
 	static void A_Star_ClosedSet(const char * filePath, const char* savePath, Heuristic heuristic, bool allowDiagonal, bool allowCross);
-	static bool A_Start_OpenSet(const char* filePath, const char* savePath, Heuristic heuristic, bool allowDiagonal, bool allowCross);
 
 	~Pathfind();
 };

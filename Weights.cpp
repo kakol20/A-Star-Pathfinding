c1e9b26aa95c5c21ff2dd2a2c653f8114847214b
Adrian <adrianaquino.aquino33@gmail.com>
2021-11-24 22:14:12 +0000

    Add "avoid turning" weight
    Also add

--- Weights.cpp
+++ Weights.cpp
@@ -4,6 +4,19 @@
 Weights::Weights() {
 }
 
+float Weights::AvoidTurning(Node* current, Node* neighbour) {
+	if (current->GetParent() != nullptr) {
+		Vector2D forward(current->GetX() - current->GetParent()->GetX(), current->GetY() - current->GetParent()->GetY());
+		forward.Normalize();
+
+		Vector2D neighbourDir(neighbour->GetX() - current->GetX(), neighbour->GetY() - current->GetY());
+		neighbourDir.Normalize();
+
+		return (1.0f - Vector2D::DotProduct(forward, neighbourDir)) * 0.1f;
+	}
+	return 0.0f;
+}
+
 float Weights::StraightTowards(Node* current, Node* neighbour, Node* end) {
 	// Current to End vector
 	Vector2D curr2End(end->GetX() - current->GetX(), end->GetY() - current->GetY());
@@ -12,7 +25,7 @@ float Weights::StraightTowards(Node* current, Node* neighbour, Node* end) {
 	Vector2D curr2Nei(neighbour->GetX() - current->GetX(), neighbour->GetY() - current->GetY());
 	curr2Nei.Normalize();
 
-	return (1.0f - Vector2D::DotProduct(curr2End, curr2Nei)) * 0.0f;
+	return (1.0f - Vector2D::DotProduct(curr2End, curr2Nei)) * 0.1f;
 }
 
 Weights::~Weights() {
--- Weights.h
+++ Weights.h
@@ -5,6 +5,7 @@ class Weights {
 public:
 	Weights();
 
+	static float AvoidTurning(Node* current, Node* neighbour);
 	static float StraightTowards(Node* current, Node* neighbour, Node* end);
 
 	~Weights();
